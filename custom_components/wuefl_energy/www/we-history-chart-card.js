a1ba0030286a46116da9c5fa3ec472206e9dc66c
Florian Wüllner <flowuellner@gmail.com>
2026-09-20 01:54:59 +0200

    Auflösung des Verlaufs an die Zähler anpassen
    Sungrow-Zähler springen in 0,1-kWh-Schritten, bei kleiner Last also nur
    alle 20 Minuten. In 5-Minuten-Abschnitten wechselten sich dadurch 0 und
    0,1 ab und das Bild zappelte. Das Diagramm misst jetzt den typischen
    Abstand zwischen zwei Zählerschritten und wählt die Schrittweite danach,
    begrenzt auf ein Vierundzwanzigstel des Zeitraums.

--- custom_components/wuefl_energy/www/we-history-chart-card.js
+++ custom_components/wuefl_energy/www/we-history-chart-card.js
@@ -69,6 +69,8 @@ class WueflEnergyHistoryCard extends WueflChartWrapper {
 
     return {
       aggregation: this._aggregation(range),
+      // Feiner geht nicht als die Zähler zählen – gröber schon, siehe we-chart
+      adaptive: true,
       y_axes: [{ unit: 'kWh' }],
       legend: [{ hidden: false, position: 'bottom-center' }],
       series,
